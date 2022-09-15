2e488fc154abbf5eebaf7b4ba39e7eb545fd57d3
Jeremiah Fallin <jeremiah.fallin@gmail.com>
2022-09-14 17:09:03 -0700

    Update ssbu.js

--- data/ssbu.js
+++ data/ssbu.js
@@ -89,6 +89,78 @@ const matches = [
     winner: "blue",
     date: "09092022",
   },
+  {
+    blue: ["Zoe"],
+    red: ["Hayden"],
+    winner: "red",
+    date: "09142022"
+  },
+  {
+    blue: ["Zoe"],
+    red: ["Chris"],
+    winner: "red",
+    date: "09142022"
+  },
+  {
+    blue: ["Zoe"],
+    red: ["Quentin"],
+    winner: "red",
+    date: "09142022"
+  },
+  {
+    blue: ["Zoe"],
+    red: ["Emma"],
+    winner: "red",
+    date: "09142022"
+  },
+  {
+    blue: ["Quentin"],
+    red: ["Conner"],
+    winner: "red",
+    date: "09142022"
+  },
+  {
+    blue: ["Emma"],
+    red: ["Conner"],
+    winner: "red",
+    date: "09142022"
+  },
+  {
+    blue: ["Hayden"],
+    red: ["Emma"],
+    winner: "blue",
+    date: "09142022"
+  },
+  {
+    blue: ["Emma"],
+    red: ["Quentin"],
+    winner: "red",
+    date: "09142022"
+  },
+  {
+    blue: ["Chris"],
+    red: ["Emma"],
+    winner: "blue",
+    date: "09142022"
+  },
+  {
+    blue: ["Chris"],
+    red: ["Quentin"],
+    winner: "blue",
+    date: "09142022"
+  },
+  {
+    blue: ["Hayden"],
+    red: ["Chris"],
+    winner: "red",
+    date: "09142022"
+  },
+  {
+    blue: ["Chris"],
+    red: ["Conner"],
+    winner: "blue",
+    date: "09142022"
+  }
 ];
 
 export default matches;
